c768c028556af5f599e84571626518fae1a7b66b
Gabe Cloud <gabecloud94@gmail.com>
2022-03-09 16:00:30 -0700

    added smooth scroll to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,40 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Header = () => (
   <div className="HeaderContainer">
     <h2 className="HeaderName">Gabe Cloud</h2>
     <ul className="NavBar">
-      <li>About</li>
-      <li>Projects</li>
-      <li>Contact</li>
+      <li>
+        <Link
+          activeClass="active"
+          to="AboutContainer"
+          smooth={true}
+          duration={1000}
+        >
+          About
+        </Link>
+      </li>
+      <li>
+        <Link
+          activeClass="active"
+          to="ProjectContainer"
+          smooth={true}
+          duration={1000}
+        >
+          Projects
+        </Link>
+      </li>
+      <li>
+        <Link
+          activeClass="active"
+          to="ContactContainer"
+          smooth={true}
+          duration={1000}
+        >
+          Contact
+        </Link>
+      </li>
     </ul>
   </div>
 );
