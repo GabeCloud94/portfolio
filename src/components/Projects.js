e266e9c942b73092bd33f10a42366a1688e9203b
Gabe Cloud <gabecloud94@gmail.com>
2022-08-22 21:11:35 -0600

    Updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,33 +7,33 @@ const Projects = () => {
       className="h-full bg-base-purple py-12 md:px-8 items-center justify-center"
     >
       <div className="flex flex-col gap-12 md:gap-8 lg:gap-16 md:flex-row items-center justify-center">
-        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 shadow-2xl hover:cursor-pointer">
+        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 hover:duration-300 shadow-2xl hover:cursor-pointer">
           <a
-            href="https://gabecloud94.github.io/Cloud-Guitars/"
+            href="https://gc-clothing.netlify.app/"
             target="_blank"
             rel="noreferrer"
           >
             <img
               className="rounded-t-2xl"
-              src={require("../images/cloud-guitars.png")}
-              alt="Cloud Guitars Website"
+              src={require("../images/gc-clothing.png")}
+              alt="GC Clothing Website"
             />
             <h3 className="text-lt-gray font-bold text-center">
-              <em>Cloud Guitars</em>
+              <em>GC Clothing</em>
             </h3>
           </a>
           <div className=" text-lt-gray p-4 lg:p-2">
             <h3 className="font-bold text-lg underline">Technologies Used</h3>
             <ul className="p-2 list-disc">
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>Vanilla JavaScript</li>
-              <li>Figma</li>
+              <li>Shopify Hydrogen</li>
+              <li>TailwindCSS</li>
+              <li>Vite</li>
+              <li>Shopify Storefront API</li>
             </ul>
           </div>
         </div>
 
-        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 shadow-2xl hover:cursor-pointer">
+        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 hover:duration-300 shadow-2xl hover:cursor-pointer">
           <a
             href="https://shareme-gcloud.netlify.app"
             target="_blank"
@@ -59,7 +59,7 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 shadow-2xl hover:cursor-pointer">
+        <div className="flex flex-col rounded-2xl w-3/5 lg:w-1/5 bg-dark-purple items-center hover:scale-110 hover:duration-300 shadow-2xl hover:cursor-pointer">
           <a
             href="https://ecommerce-sanity-stripe-ruddy.vercel.app/"
             target="_blank"
